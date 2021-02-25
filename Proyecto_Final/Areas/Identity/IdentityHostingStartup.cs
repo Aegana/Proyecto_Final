2a667c6066c9093a4a725b1f4cd635caf7527c12
aegana <arkaitzeganamartin@gmail.com>
2021-02-25 21:06:55 +0100

    Agregar archivos de proyecto.

---
+++ Proyecto_Final/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Proyecto_Final.Data;
+
+[assembly: HostingStartup(typeof(Proyecto_Final.Areas.Identity.IdentityHostingStartup))]
+namespace Proyecto_Final.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
